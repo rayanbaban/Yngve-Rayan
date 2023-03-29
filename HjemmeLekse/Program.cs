3aa911caa7927d6c7e9d6cc17b59747137c22bb5
Rayan Baban <121819849+rayanbaban@users.noreply.github.com>
2023-03-29 13:58:22 +0200

    Add files via upload
    first commit

---
+++ HjemmeLekse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace HjemmeLekse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            StreamReader reader = null;
+
+            reader = new StreamReader("C:\\GA\\MinMappe\\Persons.txt");
+            data = reader.ReadLine();
+
+            while (data != null)
+            {
+                Console.WriteLine(data);
+                data = reader.ReadLine();
+            }
+
+            List<string> list = new List<string>();
+        }
+    }
+}
